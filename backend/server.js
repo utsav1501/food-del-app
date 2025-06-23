61874df8fd40d1002b2d7275995b91c6821bfcc6
UtsavKumar01 <ukumarsingh2003@gmail.com>
2025-06-23 15:27:05 +0530

    Your message here

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ import bodyParser from "body-parser";
 
 //app config
 const app=express();
-const port=4000;
+const port=process.env.PORT||4000;
 
 //middleware
 app.use(express.json());//parse the request
